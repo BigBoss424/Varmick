ca7a2c4402640f5ea49ee013699e497438a0b2c2
bigboss424 <ajones061392@gmail.com>
2014-11-07 22:21:37 -0800

    Added Title Screen to the game.

---
+++ titleScreen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int titleScreen()
+{
+	printf("-----------------------------\n");
+	printf("Varmick\n");
+	printf("-----------------------------\n");
+	printf("\n");
+	printf("-----------------------------\n");
+	printf("The text-based Adventure Game");
+	printf("-----------------------------\n");
+
+	return 0;
+}
